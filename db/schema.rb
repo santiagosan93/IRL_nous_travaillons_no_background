f5359aadb564385c819b1ca9e60e2a85556a9585
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-09-12 19:23:56 +0200

    last fix for letter opener

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_12_142826) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "requests", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"

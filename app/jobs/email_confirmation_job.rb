ea7708245a798b7f6a90c986a79868a041b1ce7a
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-09-14 02:13:08 +0200

    I think I'll have to restart

--- app/jobs/email_confirmation_job.rb
+++
@@ -1,8 +0,0 @@
-class EmailConfirmationJob < ApplicationJob
-  queue_as :default
-
-  def perform(request_id)
-    request = Request.find(request_id)
-    RequestMailer.with(request: request).confirmation.deliver_now
-  end
-end

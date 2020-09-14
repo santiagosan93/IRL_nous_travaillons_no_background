e944e0e5fd1438b54fd3189f62cd18a6eacd34d2
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-09-14 15:35:02 +0200

    fixes workarround in tasks#seed controller'
    exit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,9 @@ class TasksController < ApplicationController
     #
     #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
     #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
     puts 'Deleting all contracts'
     Contract.delete_all
     puts "finished..."
@@ -21,7 +24,7 @@ class TasksController < ApplicationController
     17.times do
       request = Request.new(
         email: Faker::Internet.email,
-        bio: "this is a short bio about myself",
+        bio: "this is a short bio about myself and I love playing guitar",
         phone_number: '+33',
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -43,7 +46,7 @@ class TasksController < ApplicationController
     puts "Creating a request with a contract that expires in 7 days"
     request = Request.new(
       email: Faker::Internet.email,
-      bio: "this is a short bio about myself",
+      bio: "this is a short bio about myself and I love playing guitar",
       phone_number: '+33',
       first_name: "i am a contract that expires in 7 days",
       last_name: Faker::Name.last_name,
@@ -55,7 +58,7 @@ class TasksController < ApplicationController
     end
     request.assign_que_number
     request.save!
-    @contract = Contract.new(expiery_date: Date.today + 7, confirmed: false)
+    @contract = Contract.new(expiery_date: Date.today + 7, confirmed: true)
     @contract.request = request
     @contract.save
     puts "finished..."
@@ -64,7 +67,7 @@ class TasksController < ApplicationController
     puts "Creating a request with a provisional contract that expires today"
     request = Request.new(
       email: Faker::Internet.email,
-      bio: "this is a short bio about myself",
+      bio: "this is a short bio about myself and I love playing guitar",
       phone_number: '+33',
       first_name: "i am a provisional contract that expired today",
       last_name: Faker::Name.last_name,
@@ -85,7 +88,7 @@ class TasksController < ApplicationController
     puts "Creating a request with a contract that expires today and hasn't been confirmed for renewal"
     request = Request.new(
       email: Faker::Internet.email,
-      bio: "this is a short bio about myself",
+      bio: "this is a short bio about myself and I love playing guitar",
       phone_number: '+33',
       first_name: "i am a contract that expired today",
       last_name: Faker::Name.last_name,
@@ -106,7 +109,7 @@ class TasksController < ApplicationController
     puts "Creating 2 requests on que"
     request_santi = Request.new(
       email: "santi@santi.com",
-      bio: "this is a short bio about myself",
+      bio: "this is a short bio about myself and I love playing guitar",
       phone_number: '+33',
       first_name: "santiago",
       last_name: "sanchez",
@@ -122,7 +125,7 @@ class TasksController < ApplicationController
     puts "Creating a request that expires in 7 days"
     request_bea = Request.new(
       email: "bea@bea.com",
-      bio: "this is a short bio about myself",
+      bio: "this is a short bio about myself and I love playing guitar",
       phone_number: '+33',
       first_name: "i am a request that expiers in 7 days",
       last_name: "cobos",
@@ -140,62 +143,4 @@ class TasksController < ApplicationController
     puts "End of seeds"
     puts '-----------'
 
-    if Rails.env.develpment?
-      redirect_to "http://localhost:3000/letter_opener"
-    else
-      redirect_to "https://irl-nous-trvlins-no-bg-jobs.herokuapp.com/letter_opener"
-    end
-  end
-
-  def requests_send_renewal_email
-    requests = Request.unaccepted_and_still_interested
-    puts "Enqueuing confirmation of #{requests.count} requests..."
-    requests.each do |request|
-      request.confirmed = false
-      request.save
-      RequestMailer.with(request: request).request_renewal_confirmation.deliver_now
-    end
-    if Rails.env.develpment?
-      redirect_to "http://localhost:3000/letter_opener"
-    else
-      redirect_to "https://irl-nous-trvlins-no-bg-jobs.herokuapp.com/letter_opener"
-    end
-  end
-
-  def contracts_send_renewal_email
-    contracts = Contract.expires_next_week
-    puts "Enqueuing confirmation of #{contracts.count} contracts..."
-    contracts.each do |contract|
-      contract.unconfirm!
-      contract.save
-      ContractMailer.with(contract: contract).contract_renewal_confirmation.deliver_now
-    end
-    if Rails.env.develpment?
-      redirect_to "http://localhost:3000/letter_opener"
-    else
-      redirect_to "https://irl-nous-trvlins-no-bg-jobs.herokuapp.com/letter_opener"
-    end
-  end
-
-  def contracts_mark_expired
-    contracts = Contract.today_expires_and_unconfirmed
-    puts "Enqueuing expiery of #{contracts.count} contracts..."
-    contracts.each do |contract|
-      contract.expire!
-      contract.save
-      request = contract.request
-      request.remove_from_que!
-      request.unaccept!
-      request.unconfirm!
-      request.expire!
-      request.save
-      ContractMailer.with(contract: contract).contract_expired.deliver_now
-    end
-    Request.calculate_que_number
-    if Rails.env.develpment?
-      redirect_to "http://localhost:3000/letter_opener"
-    else
-      redirect_to "https://irl-nous-trvlins-no-bg-jobs.herokuapp.com/letter_opener"
-    end
-  end
 end

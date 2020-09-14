006caccefad07efa556c8d5d79041380b9e9b4ab
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-09-14 04:01:49 +0200

    im just too tired now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "https://irl-challenge-no-bg-jobs.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://irl-nous-trvlins-no-bg-jobs.herokuapp.com/" }
 
   config.action_mailer.delivery_method = :letter_opener
 
